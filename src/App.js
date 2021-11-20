d00d310df1251a0ddd92982710ac587e877cc25b
mintuka <mintuka2015@gmail.com>
2021-11-20 14:44:37 -0800

    Modified App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Forecast from "./Forecast";
+import HoursContainer from "./HoursContainer"
+import DailysContainer from "./DailysContainer"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container my-5 ">
+      <div className="row justify-content-center text-white">
+        <div className="col-md-8">
+          <div id="bg" className="card bg-image">
+            <Forecast/>
+            <HoursContainer/>
+            <DailysContainer/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
