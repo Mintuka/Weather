98e49128d5f8d3b6e207984bdc7fca6380d86bf5
mintuka <mintuka2015@gmail.com>
2021-11-20 14:48:07 -0800

    fixed some syntax errors

--- src/Daily.jsx
+++ src/Daily.jsx
@@ -1,10 +1,13 @@
 function Daily(props){
-    <div className="m-3 p-3 text-center daily" onClick={}>
-        <p><strong id={`day_${props.id}`}>Today</strong></p>
-        <img id={`icon_day_${props.id}`} alt="icon"/>
-        <p id={`main-${props.id}`}>main</p>
-        <p id={`temp-${props.id}`}>main</p>
-    </div>
+    return(
+        <div className="m-3 p-3 text-center daily">
+            <p><strong id={`day_${props.id}`}>Today</strong></p>
+            <img id={`icon_day_${props.id}`} alt="icon"/>
+            <p id={`main-${props.id}`}>main</p>
+            <p id={`temp-${props.id}`}>main</p>
+        </div>
+    )
+
 }
 
 export default Daily;
