872a0133ea460fdf6299fa1edb6f6020c4fcbd99
mintuka <mintuka2015@gmail.com>
2021-11-20 14:07:58 -0800

    created Daily component

--- src/Hour.jsx
+++ src/Hour.jsx
@@ -1,6 +1,6 @@
 function Hour(props){
     return(
-        <div id={`hr_block_${props.id}`} class="col-2" onClick={}>
+        <div id={`hr_block_${props.id}`} className="col-2" onClick={}>
             <strong id={`hr_${props.id}`} class="d-block">temp</strong>
             <img id={`icon_${props.id}`} alt="icon"/>
             <strong id={`tm_${props.id}`} class="d-block mb-2">Now</strong>
--- src/HoursContainer.jsx
+++ src/HoursContainer.jsx
@@ -4,8 +4,8 @@ import Hour from "./Hour";
 function HoursContainer(){
     const hours = useState([1,2,3,4,5,6]);
     return(
-        <div class="card-body border-top border-bottom"> 
-            <div class="row text-center">
+        <div className="card-body border-top border-bottom"> 
+            <div className="row text-center">
                 {
                     hours.map(ele => <Hour id={ele}/>)
                 }
