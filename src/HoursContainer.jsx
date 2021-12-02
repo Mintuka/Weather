6cf07dcd7068be6a6a2c2cba7b97a4e6beae50d7
mintuka <mintuka2015@gmail.com>
2021-12-02 07:25:03 -0800

    included toggle react-icon

--- src/HoursContainer.jsx
+++ src/HoursContainer.jsx
@@ -1,18 +1,34 @@
-
+import { useState } from "react";
+import {CgToggleSquare, CgToggleSquareOff} from 'react-icons/cg'
 import Hour from "./Hour";
 
 function HoursContainer(){
+    const[toggle,setToggle] = useState(false);
     const hours = [0,1,2,3,4,5];
-    return(
-        <div id="hours_container" className="card-body">
-            <b id="toggle">T</b>
-            <div className="row text-center">
-                {
-                    hours.map(ele => <Hour key={ele} id={ele}/>)
-                }
+    
+    if(!toggle)
+        return(
+            <div id="hours_container" className="card-body">
+                <CgToggleSquare id="toggle" onClick={() => setToggle(true)}>T</CgToggleSquare>
+                <div className="row text-center">
+                    {
+                        hours.map(ele => <Hour key={ele} id={ele}/>)
+                    }
+                </div>
             </div>
-        </div>
-    )
+        )
+    else{
+        return(
+            <div id="hours_container" className="card-body">
+                <CgToggleSquareOff id="toggle" onClick={() => setToggle(false)}>T</CgToggleSquareOff>
+                <br/>
+                <div className="row text-center slide">
+                    <input className="my-4" type="range" min="1" max="60"/>
+                    {hours.map(ele => <h4 className="col">{ele}PM</h4>)}
+                </div>
+            </div>
+        )   
+    }
 }
 
 export default HoursContainer;
