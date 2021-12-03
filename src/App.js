7996d6afa78a586c74cc9476fcc3a70ed7ce3393
mintuka <mintuka2015@gmail.com>
2021-12-03 08:31:40 -0800

    removed files on branch master to merge with files on branch main

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import { DailyContainer } from "./DailyContainer";
-import WeatherImage from "./WeatherImage";
-import "./App.css"
 
 function App(){
   return(
     <div>
-      <WeatherImage/>
-      <DailyContainer/>
+      
     </div>
   )
 }
