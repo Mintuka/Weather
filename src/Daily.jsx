fa0c0e474e83ff9f6b6e503898df17036d140412
mintuka <mintuka2015@gmail.com>
2021-12-03 08:19:04 -0800

    removed handleDaily and day function and added it into useFunctions.jsx

--- src/Daily.jsx
+++ src/Daily.jsx
@@ -1,32 +1,8 @@
 import { useEffect } from "react";
-import {UserContext,Url} from "./App"
+import {UserContext} from "./App"
+import {day,days,iconUrl,dataFetched,handleDaily} from "./useFunctions"
 
 function Daily(props){
-
-    const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-
-    //handle events when each daily component is clicked 
-    const handleDaily = (value) =>{
-        //off the previously clicked DailyComponent
-        let current = document.getElementsByClassName("current");
-        for(let val of current)
-            val.className = "m-3 p-3 text-center dailyWeather";
-        //on the clicked DailyComponent
-        let clicked = document.getElementById(`daily_${value}`);
-        clicked.className = "m-3 p-3 text-center current";
-
-        props.setSelectedDate(value);
-    }
-
-    const day = (val) => {
-        const dayNow = new Date().getDay();
-
-        if((val + dayNow) > 6)
-            return val + dayNow - 7; 
-        else{
-            return val + dayNow;
-        }
-    }
 
     useEffect(() => {
         document.getElementById("daily_0").className = "m-3 p-3 text-center current";
@@ -35,20 +11,15 @@ function Daily(props){
         <UserContext.Consumer>
             {
                 (data) => {
+                    var current = {};
+                    current = dataFetched(props.id,data);
+
                     return(
-                        <div id={`daily_${props.id}`} className="m-3 p-3 text-center dailyWeather" onClick={() => handleDaily(props.id)}>
+                        <div id={`daily_${props.id}`} className="m-3 p-3 text-center dailyWeather" onClick={() => handleDaily(props.id,props.setSelectedDate)}>
                             <p className="mb-1"><strong id={`day_${props.id}`}>{days[day(Number(props.id))]}</strong></p>
-                            <Url.Consumer>
-                                {
-                                    iconUrl => {
-                                        return(
-                                           data ? <img id={`icon_day_${props.id}`} className="mb-2" src={iconUrl + data.daily[props.id].weather[0].icon + ".png"} alt="icon"/> : <h3>img</h3>
-                                        )
-                                    }
-                                }
-                            </Url.Consumer>
-                            <p id={`main-${props.id}`} className="mb-3">{data ? data.daily[props.id].weather[0].main : "main"}</p>
-                            <p id={`temp-${props.id}`} className="mb-2">{data ? data.daily[props.id].temp.day : "temp"}</p>
+                            <img id={`icon_day_${props.id}`} className="mb-2" src={iconUrl + current.icon + ".png"} alt="icon"/>
+                            <p id={`main-${props.id}`} className="mb-3">{current.main}</p>
+                            <p id={`temp-${props.id}`} className="mb-2">{current.temp}</p>
                             {props.id === 0 ? <h6 id="today">Today</h6> : null}
                         </div>
                     )
